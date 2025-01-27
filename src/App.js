4e53cd6f2752fcf1f1839fa769fb3f54f67d8f4f
Derek Hargest <derek@Dereks-MacBook-Pro.local>
2025-01-27 18:05:11 -0500

    task tracker

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaskTracker from './components/TaskTracker'; // Import the TaskTracker component
+
+function App() {
+	return (
+		<div className="App">
+			<h1>AI Task Tracker</h1>
+			<TaskTracker />
+		</div>
+	);
+}
+
+export default App;
